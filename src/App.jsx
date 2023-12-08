324b8938ed93bff1dad6f21edec5cb6b65e824e7
andrew <a.kimswk@gmail.com>
2023-12-08 00:03:17 -0800

    navbar working

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,23 @@
 import { useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Home from './components/Home'
+import Gallery from './components/Gallery'
 import './styles.css'
 
 export default function App() {
   const [isLoaded, setIsLoaded] = useState(false);
   return (
-    <><nav>
-      <Navbar isLoaded={isLoaded}/>
-      </nav>
-      {isLoaded && (
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/gallery' element={<Gallery />} />
-        <Route />
-      </Routes>
-      )}
-    </>
+        <>
+        <nav>
+          <Navbar />
+        </nav>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/gallery' element={<Gallery />} />
+            <Route />
+          </Routes>
+        </>
   )
 }
 
