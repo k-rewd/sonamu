451e596ca89940d64c676b4e60dca83aba817e8e
andrew <a.kimswk@gmail.com>
2023-12-20 11:18:37 -0800

    video background working. just need editting

--- src/sections/Home/index.jsx
+++ src/sections/Home/index.jsx
@@ -1,10 +1,17 @@
 import './home.css'
 import seal from '../../assets/seal.png'
+import sonamupr from '../../assets/sonamupr.mp4'
 
 export default function Home() {
   return (
     <section id='sonamuSection'>
-      <h1 >HOME</h1>
+      {/* <h1 >HOME</h1> */}
+      {/* <div className='videoBackground'>
+        <video src={sonamupr} autoPlay loop muted />
+        <video src={sonamupr} autoPlay loop muted />
+        <video src={sonamupr} autoPlay loop muted />
+
+      </div> */}
       <img id='snm-sign' src={seal}/>
 
     </section>
