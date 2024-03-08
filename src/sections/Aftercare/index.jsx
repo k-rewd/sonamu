39d8d8d22d5c36bb2b14c93a576863283a654e29
andrew <a.kimswk@gmail.com>
2024-03-08 12:29:50 -0800

    forgot to commit eod

--- src/sections/Aftercare/index.jsx
+++ src/sections/Aftercare/index.jsx
@@ -8,29 +8,37 @@ export default function Aftercare() {
       <div className='aftercare-splash'>
         <div className='aftercare-instructions'>
           <div id='sectionAB'>
+            <h1>After</h1>
             <ul id='aftercare1'>
               <li>Keep the saniderm bandage on your tattoo for 4 to 7 days - fluid buildup during this time is normal</li>
               <li>After 4 to 7 days, remove the saniderm and wash gently with clean hands and unscented antibacterial soap</li>
               <li>Pat dry and apply a thin layer of aquaphor or a&d ointment</li>
               <li>Wash and reapply ointment in the morning and night, and periodically throughout the day as it dries out</li>
+              <li>After one week, switch to using an unscented moisturizing lotion for the next three weeks</li>
             </ul>
-            {/* <img id='imageAB'src={flash1}/> */}
-            {/* <div id='ac-sign-container'>
-            <h1 id='afterCare-sign'>AFTERCARE</h1>
-            </div> */}
           </div>
           <div id='sectionCD'>
-            {/* <img id='imageCD' src={flash2} /> */}
-            <ul id='aftercare2'>
-              <li>Take a break from any strenuous exercise to avoid trapping sweat inside the bandage</li>
-              <li>After one week, switch to using an unscented moisturizing lotion for the next three weeks</li>
-              <li>Wear clean, loose fitting clothing around your tattoo</li>
-              <li>For three weeks - Avoid baths, pools, saunas, hot tubs, oceans, tanning beds, and direct sun exposure</li>
-              <li>Do not pick, itch, scratch, or shave over your healing tattoo</li>
-            </ul>
+            <div id='aftercare2'>
+              <div className='do-card'>
+                <h2>DO</h2>
+                <ul className='do-card-info'>
+                  <li>Take a break from any strenuous exercise to avoid trapping sweat inside the bandage</li>
+                  <li>Wear clean, loose fitting clothing around your tattoo</li>
+                </ul>
+              </div>
+              <div className='dont-card'>
+                <h2>DONT</h2>
+                <ul className='dont-card-info'>
+                  <li>For three weeks - Avoid baths, pools, saunas, hot tubs, oceans, tanning beds, and direct sun exposure</li>
+                  <li>Do not pick, itch, scratch, or shave over your healing tattoo</li>
+                </ul>
+              </div>
+            </div>
+            <h1>Care</h1>
           </div>
         </div>
       </div>
     </section>
   )
 }
+
