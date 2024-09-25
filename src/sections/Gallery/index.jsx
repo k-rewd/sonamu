adbe68c216950a2cd6f1d7da1e87b8c83b939877
andrew <a.kimswk@gmail.com>
2024-09-25 15:50:26 -0700

    works

--- src/sections/Gallery/index.jsx
+++ src/sections/Gallery/index.jsx
@@ -1,117 +1,95 @@
+import { useState, useEffect } from "react";
+
 import './gallery.css'
 
 export default function Gallery() {
+  const [active, setActive] = useState(3); // Initial active slide
+  const items = [
+    {
+      title: "Slide 1",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia...",
+    },
+    {
+      title: "Slide 2",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia...",
+    },
+    {
+      title: "Slide 3",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia...",
+    },
+    {
+      title: "Slide 4",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia...",
+    },
+    {
+      title: "Slide 5",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia...",
+    },
+    {
+      title: "Slide 6",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia...",
+    },
+    {
+      title: "Slide 7",
+      content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia...",
+    },
+  ];
+
+  const loadShow = () => {
+    return items.map((_, index) => {
+      if (index === active) {
+        return {
+          transform: 'none',
+          zIndex: 1,
+          filter: 'none',
+          opacity: 1,
+        };
+      } else if (index > active) {
+        const stt = index - active;
+        return {
+          transform: `translateX(${120 * stt}px) scale(${1 - 0.2 * stt}) perspective(16px) rotateY(-1deg)`,
+          // zIndex: -stt,
+          filter: 'blur(5px)',
+          opacity: stt > 2 ? 0 : 0.6,
+        };
+      } else {
+        const stt = active - index;
+        return {
+          transform: `translateX(${-120 * stt}px) scale(${1 - 0.2 * stt}) perspective(16px) rotateY(1deg)`,
+          // zIndex: -stt,
+          filter: 'blur(5px)',
+          opacity: stt > 2 ? 0 : 0.6,
+        };
+      }
+    });
+  };
+
+  useEffect(() => {
+    loadShow();
+  }, [active]);
+
+  const nextSlide = () => {
+    setActive((prevActive) => (prevActive + 1 < items.length ? prevActive + 1 : prevActive));
+  };
+
+  const prevSlide = () => {
+    setActive((prevActive) => (prevActive - 1 >= 0 ? prevActive - 1 : prevActive));
+  };
+
+  const styles = loadShow();
+
   return (
     <section id='gallerySection'>
-      <h1>GALLERY</h1>
       <div className='slider'>
-        <div className='item'>
-          <h1>Slide 1</h1>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-          molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-          numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-          optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
-          obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
-          nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-          tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
-          quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
-          sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
-          recusandae alias error harum maxime adipisci amet laborum.
-        </div>
-        <div className='item'>
-          <h1>Slide 1</h1>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-          molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-          numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-          optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
-          obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
-          nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-          tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
-          quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
-          sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
-          recusandae alias error harum maxime adipisci amet laborum.
-        </div>
-        <div className='item'>
-          <h1>Slide 2</h1>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-          molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-          numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-          optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
-          obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
-          nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-          tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
-          quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
-          sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
-          recusandae alias error harum maxime adipisci amet laborum.
-        </div>
-        <div className='item'>
-          <h1>Slide 3</h1>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-          molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-          numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-          optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
-          obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
-          nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-          tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
-          quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
-          sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
-          recusandae alias error harum maxime adipisci amet laborum.
-        </div>
-        <div className='item'>
-          <h1>Slide 4</h1>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-          molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-          numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-          optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
-          obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
-          nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-          tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
-          quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
-          sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
-          recusandae alias error harum maxime adipisci amet laborum.
-        </div>
-        <div className='item'>
-          <h1>Slide 5</h1>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-          molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-          numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-          optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
-          obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
-          nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-          tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
-          quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
-          sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
-          recusandae alias error harum maxime adipisci amet laborum.
-        </div>
-        <div className='item'>
-          <h1>Slide 6</h1>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-          molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-          numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-          optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
-          obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
-          nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-          tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
-          quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
-          sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
-          recusandae alias error harum maxime adipisci amet laborum.
-        </div>
-        <div className='item'>
-          <h1>Slide 7</h1>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,
-          molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum
-          numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium
-          optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis
-          obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam
-          nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit,
-          tenetur error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
-          quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias eos
-          sapiente officiis modi at sunt excepturi expedita sint? Sed quibusdam
-          recusandae alias error harum maxime adipisci amet laborum.
-        </div>
-        
-
+        {items.map((item, index) => (
+          <div className='item' key={index} style={styles[index]}>
+            <h1>{item.title}</h1>
+            <p>{item.content}</p>
+          </div>
+        ))}
       </div>
+      <button id="prev" onClick={prevSlide}>Prev</button>
+      <button id="next" onClick={nextSlide}>Next</button>
     </section>
-  )
-}
+  );
+};
